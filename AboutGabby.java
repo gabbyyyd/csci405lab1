f28e6b5daec334dc75e5ce9bbd6e2a65ec0ffaa1
gabbyyyd <gabby.davison03@gmail.com>
2025-08-31 16:22:19 -0400

    Add bio and AboutGabby program

---
+++ AboutGabby.java
@@ -0,0 +1,11 @@
+public class AboutGabby {
+    public static void main(String[] args) {
+        String name = "Gabby Davison";
+        String major = "Computer Science";
+        String futureJob = "a cybersecurity company in New Jersey";
+
+        System.out.println("Name: " + name);
+        System.out.println("Major: " + major);
+        System.out.println("Expected First Job: " + futureJob);
+    }
+}
