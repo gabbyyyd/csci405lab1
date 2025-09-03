f2fcb29cbf38f88f845f631d6e57839e2a23f3b8
gabbyyyd <gabby.davison03@gmail.com>
2025-09-03 16:49:29 -0400

    Update AboutGabby.java

--- AboutGabby.java
+++ AboutGabby.java
@@ -1,11 +1,8 @@
 public class AboutGabby {
     public static void main(String[] args) {
-        String name = "Gabby Davison";
-        String major = "Computer Science";
-        String job = "a cybersecurity company in New Jersey";
-
-        System.out.println("Name: " + name);
-        System.out.println("Major: " + major);
-        System.out.println("Expected First Job: " + job);
+        
+        System.out.println("Name: Gabby Davison");
+        System.out.println("Major: Computer Science");
+        System.out.println("Expected First Job: a cybersecurity company in New Jersey");
     }
 }
