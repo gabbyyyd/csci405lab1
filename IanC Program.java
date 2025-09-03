90c3cc068cd82684b754d0591c5353b28f81a0c8
IanJC86 <ic001@marietta.edu>
2025-09-03 16:48:16 -0400

    Bio and java program

---
+++ IanC Program.java
@@ -0,0 +1,5 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Name: Ian Carpenter, Major: Computer Science, Job after Graduation: I'm not too sure what job I will get but I'm thinking of starting out in IT at a company then working my way up the ladder to specialize in Networking or Databases, or maybe even a PM");
+    }
+}
