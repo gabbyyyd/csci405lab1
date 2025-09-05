37279cd6b71e349fd2e00384e4835cf8073771cc
TReich34 <163215939+TReich34@users.noreply.github.com>
2025-09-04 19:44:34 -0700

    Add files via upload

---
+++ TylerBio.java
@@ -0,0 +1,9 @@
+public class TylerBio {
+    public static void main(String[] args) {
+    
+        System.out.printIn("Name: Tyler Reich");
+        System.out.printIn("Major: Information Systems");
+        System.out.printIn("Expected First Job: A comm job within the U.S Airforce");
+
+    }
+}
