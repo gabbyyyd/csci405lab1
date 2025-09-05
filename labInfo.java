f9a2a88788dd8988df560549ff4e3c8197eb0a70
landonharney <landon.harney1@gmail.com>
2025-09-04 21:19:24 -0400

    Add files via upload

---
+++ labInfo.java
@@ -0,0 +1,11 @@
+public class labInfo {
+	public static void main(String[] args) {
+		String myName = “Landon Harney”;
+		String myMajor = “Computer Science”;
+		String myFirstJob = “Data Scientist for Abercrombie & Fitch”;
+
+		System.out.println(“Name: “ + myName);
+		System.out.println(“Major: “ + myMajor);
+		System.out.println(“First Job: “ + myFirstJob);
+	}
+}
