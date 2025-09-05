d8702a88822deee7202dcef5608f6d6b13c1080d
TReich34 <163215939+TReich34@users.noreply.github.com>
2025-09-04 19:57:27 -0700

    Update TylerBio.java

--- TylerBio.java
+++ TylerBio.java
@@ -1,9 +1,10 @@
 public class TylerBio {
     public static void main(String[] args) {
     
-        System.out.printIn("Name: Tyler Reich");
-        System.out.printIn("Major: Information Systems");
-        System.out.printIn("Expected First Job: A comm job within the U.S Airforce");
+        System.out.println("Name: Tyler Reich");
+        System.out.println("Major: Information Systems");
+        System.out.println("Expected First Job: A comm job within the U.S Airforce");
 
     }
+
 }
